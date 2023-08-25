b31c0e8eff068bb35f78cc12aa0eb04c2c201b21
rar <rahulraut430@gmail.com>
2023-08-26 02:40:29 +0530

    typescript configs

---
+++ index.ts
@@ -0,0 +1,9 @@
+/**
+ * This function prints all the car details to the console.
+ * @param make - This is the company that manufactured the car.
+ * @param model - The year of manufacture.
+ * @param sedan - Indicated whether the car is a sedan or not.
+ */
+let test:string = "Rahul";
+
+console.log(test);
