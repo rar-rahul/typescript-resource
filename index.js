f2b7db49957eb322e0d9056b8a484a6357be3f54
rar <rahulraut430@gmail.com>
2023-08-26 14:55:16 +0530

    typescript changes

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-/**
- * This function prints all the car details to the console.
- * @param make - This is the company that manufactured the car.
- * @param model - The year of manufacture.
- * @param sedan - Indicated whether the car is a sedan or not.
- */
-var test = "Rahul";
-console.log(test);
+
+const carModel = (model:number) => { 
+    console.log(model)
+}
+
+carModel(2010)
--- index.ts
+++ index.ts
@@ -4,6 +4,19 @@
  * @param model - The year of manufacture.
  * @param sedan - Indicated whether the car is a sedan or not.
  */
-let test:string = "Rahul";
+function printModel(model:number[]):number{
 
-console.log(test);
+    model.forEach(model => { 
+        console.log(model)
+    })
+
+    let newModel = model[0] + 1;
+
+    return newModel
+
+}
+
+let upcomingModel = printModel([1001,1002,1003])
+
+console.log(upcomingModel)
+
